4121f9dafba3419f26bf0075d33b604e2061730e
Manuel Dios <mdios@utem.cl>
2025-01-21 02:25:08 -0300

    Easy

--- Easy/1.py
+++ Easy/1.py
@@ -1,2 +1,9 @@
-#Suma de dos números
-#Escribe un programa que solicite al usuario ingresar dos números y luego imprima su suma.
+# Suma de dos números
+# Escribe un programa que solicite al usuario ingresar dos números y luego imprima su suma.
+
+num1 = int(input("Ingrese un numero: "))
+num2 = int(input("Ingrese otro numero: "))
+
+suma = num1 + num2
+
+print(f"La suma entre los dos numeros es: {suma}")
--- Easy/2.py
+++ Easy/2.py
@@ -1,1 +1,9 @@
-#Escribe un programa que imprima todos los números pares desde 1 hasta un número N ingresado por el usuario.
+# Programa que imprime todos los números pares desde 1 hasta N
+
+# Solicitar el número N al usuario
+num = int(input("Ingrese un número: "))
+
+# Iterar desde 1 hasta N
+print(f"Números pares desde 1 hasta {num}:")
+for i in range(2, num + 1, 2):  # Comenzar en 2, incrementar de 2 en 2
+    print(i)
