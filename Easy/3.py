7f51c0c31e36732f2fa552ea49fe4f516a103c0a
Manuel Dios <mdios@utem.cl>
2025-01-22 21:28:19 -0300

    3.py listo

--- Easy/3.py
+++ Easy/3.py
@@ -1,2 +1,9 @@
 #Escribe un programa que convierta una temperatura en grados Celsius a Fahrenheit. Usa la fórmula:
 # F=C×95+32F=C×59​+32.
+
+num_grados = float(input("Ingrese un valor (en grados): "))
+
+num_far = ((num_grados * (9/5)) + 32)
+
+print(f"Grados en fahrenheit: {num_far}")
+
