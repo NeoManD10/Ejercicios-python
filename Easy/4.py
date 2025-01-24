01199b1722712dc2ae62536ca9393380b7ef1295
Manuel Dios <mdios@utem.cl>
2025-01-24 04:40:05 -0300

    4.py

--- Easy/4.py
+++ Easy/4.py
@@ -1,1 +1,9 @@
 #Escribe un programa que tome una cadena ingresada por el usuario y la imprima al revés.
+
+cadena = input("Ingresa una cadena: ")
+
+# Invertir la cadena
+cadena_invertida = cadena[::-1] #utiliza el operador de slicing para recorrer la cadena en orden inverso.
+
+# Imprimir la cadena invertida
+print("La cadena invertida es:", cadena_invertida)
