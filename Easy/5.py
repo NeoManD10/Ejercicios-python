6b5a9eb8e146180fce5883d218403eda42484cdc
Manuel Dios <mdios@utem.cl>
2025-01-25 20:26:35 -0300

    5.py

--- Easy/5.py
+++ Easy/5.py
@@ -5,3 +5,17 @@
 # Lista: [1, 5, 8, 10, 3]
 # Valor: 5
 # Salida esperada: 8, 10
+
+# Solicitar al usuario que ingrese una lista de números
+lista = input("Ingresa una lista de números separados por comas: ")
+lista = [int(x) for x in lista.split(",")]
+
+# Solicitar al usuario que ingrese el valor N
+n = int(input("Ingresa el valor N: "))
+
+# Filtrar los números mayores que N
+numeros_mayores = [x for x in lista if x > n]
+
+# Imprimir los números mayores que N
+print("Números mayores que", n, ":", ", ".join(map(str, numeros_mayores)))
+
