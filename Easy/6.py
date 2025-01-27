a7ea84c3c7802f6a0f8b9061cac501504e43df7c
Manuel Dios <mdios@utem.cl>
2025-01-27 02:10:52 -0300

    6.py

--- Easy/6.py
+++ Easy/6.py
@@ -1,2 +1,14 @@
 #Realiza un programa que calcule el numero factorial de un numero
 #Usa recursividad
+
+print("Calcular el numero factorial de un numero")
+n = int(input("Ingresa un numero: "))
+
+def fact(n):
+    if n == 1:
+        return 1
+    else:
+        return n * fact(n - 1)
+    
+print(fact(n))
+
