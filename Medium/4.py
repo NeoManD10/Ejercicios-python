b16c6f7a457a2230cbbccaa9d472b37b73e26b14
Manuel Dios <mdios@utem.cl>
2025-02-04 02:04:35 -0300

    Medium/4.py

--- Medium/4.py
+++ Medium/4.py
@@ -4,3 +4,16 @@
 # Entrada esperada:
 # Ingresa el número de términos: 6
 # Salida esperada: 0, 1, 1, 2, 3, 5
+
+numero = int(input("Ingresa un numero: "))
+
+def fibo(numero):
+    lista = [0,1]
+    i = 2
+
+    while i < numero:
+        lista.append(lista[i-2] + lista[i-1])
+        i += 1
+    return lista
+
+print(fibo(numero))
