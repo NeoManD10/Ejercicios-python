a97e0def4886669d7768fb88e5ee0e7857ff4c27
Manuel Dios <mdios@utem.cl>
2025-02-04 02:28:48 -0300

    Medium/5.py

--- Medium/5.py
+++ Medium/5.py
@@ -4,3 +4,20 @@
 # Entrada esperada:
 # Ingresa una lista de números separados por comas: 5,2,9,1,5
 # Salida esperada: [1, 2, 5, 5, 9]
+
+lista = input("Ingresa una lista de números separados por comas: ")
+lista = [int(x) for x in lista.split(",")]
+
+#Usando sorted
+lista_ordenada = sorted(lista)
+
+print(lista_ordenada)
+
+# Implementación del algoritmo de burbuja
+for i in range(len(lista)):
+    for j in range(0, len(lista) - i - 1):
+        if lista[j] > lista[j + 1]:
+            # Intercambiar elementos si están en el orden incorrecto
+            lista[j], lista[j + 1] = lista[j + 1], lista[j]
+
+print("Lista ordenada de menor a mayor:", lista)
